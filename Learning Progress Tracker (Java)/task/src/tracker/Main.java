6359d54c89d5bbf0ca398017b42ad1e3df40cccb
Yaroslav Sliesar <super.batup2013@gmail.com>
2025-02-12 21:38:30 +0200

    add implemented step 2 and this is a verified implementation

--- Learning Progress Tracker (Java)/task/src/tracker/Main.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -1,7 +1,5 @@
 package tracker;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         LearningProgressTracker learningProgressTracker = new LearningProgressTracker();
